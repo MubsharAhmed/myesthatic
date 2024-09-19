be090aa64bdd63cea3bf68278308e31e70c05994
malik250danish <danish250ahmad@gmail.com>
2024-09-19 18:56:18 +0500

    patient module

---
+++ application/views/frontend/patients.php
@@ -0,0 +1,217 @@
+<!--start main wrapper-->
+<main class="main-wrapper">
+    <div class="main-content">
+
+        <div class="row">
+            <div class="col-md-4 my-2">
+                <h3 class="fmon">Patients</h3>
+            </div>
+            <div class="col-md-8 my-2">
+                <div class="row">
+
+                    <div class="col-md-3">
+                        <a href="<?php echo base_url(); ?>addPatient" class="btn  btnAdd w-100">Add Patient</a>
+                    </div>
+                    <div class="col-md-3">
+                        <label for="csv" class="btn  btnAdd w-100">Upload CSV</label>
+                        <input type="file" accept=".csv" name="" id="csv" class="d-none">
+                    </div>
+                    <div class="col-md-3 my-1">
+                        <select name="" id="" class="form-select fsele">
+                            <option value="" selected>Today</option>
+                            <option value="">1</option>
+                            <option value="">2</option>
+                            <option value="">2</option>
+                        </select>
+                    </div>
+                    <div class="col-md-3 my-1">
+                        <select name="" id="" class="form-select fsele ">
+                            <option value="" selected>Filter</option>
+                            <option value="">1</option>
+                            <option value="">2</option>
+                            <option value="">2</option>
+                        </select>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="mCard  ">
+
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Phone</th>
+                                    <th>Date of birth</th>
+                                    <th>Location</th>
+                                    <th>Procedures</th>
+
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!-- Sample data -->
+                                <tr>
+                                    <td class="d-flex gap-1 align-items-center">
+
+                                        Ronaldo
+                                    </td>
+                                    <td>Example@info.com</td>
+                                    <td>+98 765 4321 0</td>
+                                    <td>2024-04-17</td>
+                                    <td>Location A</td>
+                                    <td class="fw-bold text-black">
+                                        <a class="text-black" href="./procedurereports.html">View</a>
+                                    </td>
+
+                                    <td>
+                                        <div class="elep p-2 ">
+                                            <img src="<?php echo base_url(); ?>assets/images/ellep.png" alt="">
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="d-flex gap-1 align-items-center">
+
+                                        Ronaldo
+                                    </td>
+                                    <td>Example@info.com</td>
+                                    <td>+98 765 4321 0</td>
+                                    <td>2024-04-17</td>
+                                    <td>Location A</td>
+                                    <td class="fw-bold text-black">
+                                        <a class="text-black" href="./procedurereports.html">View</a>
+                                    </td>
+
+
+                                    <td>
+                                        <div class="elep p-2 ">
+                                            <img src="<?php echo base_url(); ?>assets/images/ellep.png" alt="">
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="d-flex gap-1 align-items-center">
+
+                                        Ronaldo
+                                    </td>
+                                    <td>Example@info.com</td>
+                                    <td>+98 765 4321 0</td>
+                                    <td>2024-04-17</td>
+                                    <td>Location A</td>
+                                    <td class="fw-bold text-black">
+                                        <a class="text-black" href="./procedurereports.html">View</a>
+                                    </td>
+
+
+                                    <td>
+                                        <div class="elep p-2 ">
+                                            <img src="<?php echo base_url(); ?>assets/images/ellep.png" alt="">
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="d-flex gap-1 align-items-center">
+
+                                        Ronaldo
+                                    </td>
+                                    <td>Example@info.com</td>
+                                    <td>+98 765 4321 0</td>
+                                    <td>2024-04-17</td>
+                                    <td>Location A</td>
+                                    <td class="fw-bold text-black">
+                                        <a class="text-black" href="./procedurereports.html">View</a>
+                                    </td>
+
+
+                                    <td>
+                                        <div class="elep p-2 ">
+                                            <img src="<?php echo base_url(); ?>assets/images/ellep.png" alt="">
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="d-flex gap-1 align-items-center">
+
+                                        Ronaldo
+                                    </td>
+                                    <td>Example@info.com</td>
+                                    <td>+98 765 4321 0</td>
+                                    <td>2024-04-17</td>
+                                    <td>Location A</td>
+                                    <td class="fw-bold text-black">
+                                        <a class="text-black" href="./procedurereports.html">View</a>
+                                    </td>
+
+
+                                    <td>
+                                        <div class="elep p-2 ">
+                                            <img src="<?php echo base_url(); ?>assets/images/ellep.png" alt="">
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="d-flex gap-1 align-items-center">
+
+                                        Ronaldo
+                                    </td>
+                                    <td>Example@info.com</td>
+                                    <td>+98 765 4321 0</td>
+                                    <td>2024-04-17</td>
+                                    <td>Location A</td>
+                                    <td class="fw-bold text-black">
+                                        <a class="text-black" href="./procedurereports.html">View</a>
+                                    </td>
+
+
+                                    <td>
+                                        <div class="elep p-2 ">
+                                            <img src="<?php echo base_url(); ?>assets/images/ellep.png" alt="">
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="d-flex gap-1 align-items-center">
+
+                                        Ronaldo
+                                    </td>
+                                    <td>Example@info.com</td>
+                                    <td>+98 765 4321 0</td>
+                                    <td>2024-04-17</td>
+                                    <td>Location A</td>
+                                    <td class="fw-bold text-black">
+                                        <a class="text-black" href="./procedurereports.html">View</a>
+                                    </td>
+
+
+                                    <td>
+                                        <div class="elep p-2 ">
+                                            <img src="<?php echo base_url(); ?>assets/images/ellep.png" alt="">
+                                        </div>
+                                    </td>
+                                </tr>
+
+                                <!-- More rows here -->
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</main>
+<!--end main wrapper-->
+
+
+<!--start overlay-->
+<div class="overlay btn-toggle"></div>
+<!--end overlay-->
