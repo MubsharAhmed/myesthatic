b352eb00776a41cd82775a1c7cd17c3ade795747
MubsharAhmed <142440906+MubsharAhmed@users.noreply.github.com>
2024-09-19 18:58:08 +0500

    view functions

--- application/controllers/General.php
+++ application/controllers/General.php
@@ -7,7 +7,49 @@ class General extends BaseController
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('general_model');
+        // $this->load->model('general_model');
+    }
+
+    public function patients(){
+        if (!$this->isAdmin()) {
+            redirect('login');
+        }
+        $this->global['pageTitle'] = 'My Esthatic : Patients';
+
+        $this->loadViews("frontend/patietns", $this->global, NULL);
+
+
+
+    }
+
+    public function addPatient(){
+        if (!$this->isAdmin()) {
+            redirect('login');
+        }
+        $this->global['pageTitle'] = 'My Esthatic : Add Patients';
+
+        $this->loadViews("frontend/addpatient", $this->global, NULL);
+
+    }
+
+    public function procedureReports(){
+        if (!$this->isAdmin()) {
+            redirect('login');
+        }
+        $this->global['pageTitle'] = 'My Esthatic : Procedure reports';
+
+        $this->loadViews("frontend/procedurereports", $this->global, NULL);
+
+    }
+
+    public function pReports(){
+        if (!$this->isAdmin()) {
+            redirect('login');
+        }
+        $this->global['pageTitle'] = 'My Esthatic : Procedure reports';
+
+        $this->loadViews("frontend/preport", $this->global, NULL);
+
     }
 
 
